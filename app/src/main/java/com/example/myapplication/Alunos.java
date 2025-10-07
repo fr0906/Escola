be506d5982dc829884a5f52b8570140e7ca9444f
franciellysouzax@gmail.com <fr0906>
2025-10-07 20:23:37 -0300

    Criado classe alunos

---
+++ app/src/main/java/com/example/myapplication/Alunos.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+public class Alunos {
+    String nomeAt;
+    String cursoAt;
+}
+
+public Alunos(String namePar, String coursePar){
+    this.nomeAt = namePar;
+    this.cursoAt = coursePar;
+}
+
+public String getNome(){
+    return nomeAt;
+}
+
+public String getCurso(){
+    return cursoAt;
+}
